318d5379ab0d602a7a5287580243432dcc52c217
ortizgreg52 <ortizgreg52@yahoo.com>
2016-10-03 16:20:12 -0500

    Add files via upload

--- XMLReader.cs
+++ XMLReader.cs
@@ -13,8 +13,41 @@ namespace XMLTest
         static void Main(string[] args)
         {
             XmlDocument xmlDoc = new XmlDocument();
-            xmlDoc.Load("XML_FF.xml");
-            XmlNodeList prog = xmlDoc.
+            xmlDoc.Load("C:\\Users\\gbortiz\\Desktop\\XML_FF.xml");
+            //XmlNode node = xmlDoc.DocumentElement.SelectSingleNode("/install/program");
+            //Console.WriteLine(xmlDoc.DocumentElement.OuterXml);
+            XmlNode root = xmlDoc.FirstChild;
+            XmlNode elm = root;
+            Node xmlNode = new Node();
+            List<Node> xmlNode_list = new List<Node>();
+            //Console.WriteLine(elm);
+            if (root.HasChildNodes)
+             {
+                 for (int i = 0; i < elm.ChildNodes.Count; i++)
+                 {
+                    Console.WriteLine(i);
+                    Console.WriteLine(elm.ChildNodes.Count);
+                    xmlNode.Elm_Name = elm.ChildNodes[i].Name;
+                    xmlNode.Data = elm.ChildNodes[i].InnerText;
+                    Console.WriteLine(xmlNode.Elm_Name);
+                    Console.WriteLine(": ");
+                    Console.WriteLine(xmlNode.Data);
+                    Console.WriteLine();
+                 }
+             }
+        }
+    }
+
+    public class Node
+    {
+        public String Elm_Name;
+        public Char Type;
+        public String Data;
+        public Node()
+        {
+            Elm_Name = " ";
+            Type = ' ';
+            Data = " ";
         }
     }
 }
