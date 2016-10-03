d094a9fd3828ba368c9f8a655671e29c21fab2d8
ortizgreg52 <ortizgreg52@yahoo.com>
2016-10-03 12:27:45 -0500

    Add files via upload

---
+++ DownloadInstallPage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Pull_HTML_test
+{
+    class Program
+    {
+
+        public static void Download_Exe(string url)
+        {
+            WebClient Client = new WebClient();
+            Client.DownloadFile(new Uri(url), "C:\\Users\\gbortiz\\Downloads\\FF.exe");
+        }
+
+        public static void Download_Page(string url)
+        {
+            WebClient Client = new WebClient();
+            Client.DownloadFile(new Uri(url), "C:\\Users\\gbortiz\\html.txt");
+        }
+
+        static void Main(string[] args)
+        {
+            Download_Page("https://www.mozilla.org/en-US/firefox/all/");
+
+            using (StreamReader sr = new StreamReader("C:\\Users\\gbortiz\\html.txt"))
+            {
+                String line = sr.ReadToEnd();
+                int temp = line.IndexOf("class=\"download win64\"");
+                Console.WriteLine(temp);
+                line = line.Substring(temp);
+                temp = line.IndexOf("en-US");
+                Console.WriteLine(temp);
+                line = line.Substring(temp);
+                temp = line.IndexOf("class=\"download win64\"");
+                Console.WriteLine(temp);
+                line = line.Substring(temp);
+                Match m = Regex.Match(line, @"href=""[^""\\]*(?:\\.[^""\\]*)*""");
+                line = m.ToString();
+                m = Regex.Match(line, @"http.*");
+                line = m.ToString();
+                line = line.Trim('"');
+                Console.WriteLine(line);
+                Download_Exe(line);
+            }
+        }
+    }
+}
---
+++ XMLReader.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.IO;
+
+namespace XMLTest
+{
+    class XMLReader
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load("XML_FF.xml");
+            XmlNodeList prog = xmlDoc.
+        }
+    }
+}
